76297cb865e2f1add9209dc4080477d49de044b1
mayco <mayco@DESKTOP-RCDVL0R.localdomain>
2024-08-14 11:16:04 -0300

    Exercicio finalizado

--- ContaBancaria/src/ContaTerminal.java
+++ ContaBancaria/src/ContaTerminal.java
@@ -1,8 +1,34 @@
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-		System.out.println("ola");
+		/*
+		 * importar a classe Scaner Exibir as msg para os usuarios informar os dados
+		 * obter pelo scanner os valores diigitados apresentar os valores digitados
+		 */
+		String mensagem;
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+		System.out.println("Por favor, digite o número da Agência !");
+		String agencia = scanner.next();
+
+		System.out.println("Por favor, digite o número da Conta !");
+		int numero = scanner.nextInt();
+
+		System.out.println("Por favor, digite seu nome !");
+		String nome = scanner.next().toUpperCase();
+
+		System.out.println("Por favor, digite o Saldo da Conta!");
+		double saldo = scanner.nextDouble();
+
+		mensagem = "Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência e: " + agencia
+	                      + ", conta: " + numero + " e seu saldo: " + saldo + "já está disponível para saque";
+ 
+		System.out.println(mensagem);
+		
 	}
 
 }
